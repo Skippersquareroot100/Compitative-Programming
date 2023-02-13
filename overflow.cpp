a59bfdd7e7d91185c3c3860b6e2a5a8889e6b770
Maruf Hasan Anik <111722365+Skippersquareroot100@users.noreply.github.com>
2023-02-14 01:12:57 +0600

    Add files via upload

---
+++ overflow.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a=7e18+550;
+    int c=100000;
+    int b=100000;
+    cout<<a<<endl;
+    cout<<fixed<<a<<endl;
+    cout<<fixed<<setprecision(0)<<a<<endl;
+    cout<<fixed<<setprecision(2)<<a<<endl;
+    cout<<c*b<<endl;
+    cout<<c*1LL*b<<endl;
+    
+
+/* --int (rough range): -10^9 to 10^9.
+-long int (rough range): -10^12 to 10^12.
+--long long int (rough range): -10^18 to 10^18.*/
+
+}
